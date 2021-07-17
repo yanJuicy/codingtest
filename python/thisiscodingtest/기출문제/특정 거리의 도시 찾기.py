5a88755f0a95ddde58d4b494e30588337d41bbcc
yanJuicy <hanjub3@gmail.com>
2021-07-18 06:02:38 +0900

    thisiscodingtest

--- python/thisiscodingtest/기출문제/특정 거리의 도시 찾기.py
+++ python/thisiscodingtest/기출문제/특정 거리의 도시 찾기.py
@@ -1,42 +1,35 @@
 n, m, k, x = map(int, input().split())
 
-graph = [[] for i in range(n + 1)]
+graph = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
+distance = [0] * (n + 1)
 
 for i in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-result = []
-
 from collections import deque
 
-def bfs(s, level):
+def bfs(s):
     q = deque()
     q.append(s)
     visited[s] = True
 
     while q:
-        length = len(q)
+        cur = q.popleft()
 
-        while length > 0:
-            length -= 1
-            cur = q.popleft()
-
-            if level == 0:
-                result.append(cur)
-
-            for nextNode in graph[cur]:
-                if not visited[nextNode]:
-                    q.append(nextNode)
-                    visited[nextNode] = True
-        level -= 1
-
-
-bfs(x, k)
-
-if len(result) == 0: print(-1)
-else: 
-    sorted(result)
-    for i in result:
+        for n in graph[cur]:
+            if not visited[n]:
+                visited[n] = True
+                q.append(n)
+                distance[n] = distance[cur] + 1
+    
+bfs(x)
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
         print(i)
+        check = True
+
+if not check: 
+    print(-1)
