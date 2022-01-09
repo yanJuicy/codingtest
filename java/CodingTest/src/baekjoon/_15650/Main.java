d420fd420f367cb6f6ae9be20724836eee59a104
yanJuicy <hanjub3@gmail.com>
2022-01-09 23:51:36 +0900

    [BOJ] 15650 N과M (4) 5분

---
+++ java/CodingTest/src/baekjoon/_15650/Main.java
@@ -0,0 +1,39 @@
+package baekjoon._15650;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int N, M;
+    static int selected[];
+    static StringBuilder sb;
+
+    static void input() {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        selected = new int[N + 1];
+        sb = new StringBuilder();
+    }
+
+    static void solve(int k) {
+        if (k == M + 1) {
+            for (int i=1; i<=M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int cand = selected[k - 1] + 1; cand<=N; cand++) {
+                selected[k] = cand;
+                solve(k + 1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        solve(1);
+        System.out.println(sb.toString());
+    }
+}
