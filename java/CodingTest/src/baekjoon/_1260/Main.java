61d51dc6e10681da078ee2ea8d0e7f7cc990b015
yanJuicy <hanjub3@gmail.com>
2022-01-14 04:40:03 +0900

    reflect

--- java/CodingTest/src/baekjoon/_1260/Main.java
+++ java/CodingTest/src/baekjoon/_1260/Main.java
@@ -3,7 +3,12 @@ package baekjoon._1260;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
