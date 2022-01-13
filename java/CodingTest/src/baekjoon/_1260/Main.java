592199fc960dacbef76fd2a2afaada5a5e5d3521
yanJuicy <hanjub3@gmail.com>
2022-01-14 04:37:11 +0900

    [BOJ] DFS와 BFS 20분

--- java/CodingTest/src/baekjoon/_1260/Main.java
+++ java/CodingTest/src/baekjoon/_1260/Main.java
@@ -1,11 +1,11 @@
-package baekjoon;
+package baekjoon._1260;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class p_1260 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
