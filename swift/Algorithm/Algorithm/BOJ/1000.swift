53d601b81307748fcf7ec471af7d43135fbce2da
yanJuicy <hanjub3@gmail.com>
2022-01-24 09:49:29 +0900

    swift initial commit

---
+++ swift/Algorithm/Algorithm/BOJ/1000.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 이한주 on 2022/01/17.
+//
+
+import Foundation
+
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+print(Int(lineArr[0])! + Int(lineArr[1])!)
+
+
