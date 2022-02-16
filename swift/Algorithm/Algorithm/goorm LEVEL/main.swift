4f497be3207a5c2ca7094259bcee724cfc8bcc41
yanJuicy <hanjub3@gmail.com>
2022-02-17 03:33:03 +0900

    goorm level

--- swift/Algorithm/Algorithm/goorm LEVEL/main.swift
+++ swift/Algorithm/Algorithm/goorm LEVEL/main.swift
@@ -1,8 +1,25 @@
-//
-//  main.swift
-//  Algorithm
-//
-//  Created by 이한주 on 2022/01/31.
-//
-
 import Foundation
+
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+
+let kor = input[0]
+let eng = input[1]
+let math = input[2]
+
+let avg: Double = Double(kor + eng + math) / 3
+
+var res = String(format: "%.2f", avg)
+
+if avg > 90 {
+    res = "\(res) A"
+} else if avg > 80 {
+    res = "\(res) B"
+}else if avg > 70 {
+    res = "\(res) C"
+}else if avg > 60 {
+    res = "\(res) D"
+} else {
+    res = "\(res) F"
+}
+
+print(res)
