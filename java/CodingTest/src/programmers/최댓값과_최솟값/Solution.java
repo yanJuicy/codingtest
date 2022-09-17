6fdc6517c5b26203e8a9d16e3d38f899c357ed54
yanJuicy <hanjub3@gmail.com>
2022-09-18 00:44:59 +0900

    [programmers] 최댓값과 최솟값 10분

---
+++ java/CodingTest/src/programmers/최댓값과_최솟값/Solution.java
@@ -0,0 +1,21 @@
+package programmers.최댓값과_최솟값;
+
+public class Solution {
+    public String solution(String s) {
+        String[] inputs = s.split(" ");
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (String str : inputs) {
+            int n = Integer.parseInt(str);
+            if (max < n) {
+                max = n;
+            }
+            if (min > n) {
+                min = n;
+            }
+        }
+        String answer = min + " " + max;
+        return answer;
+    }
+}
