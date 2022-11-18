76b910887eeb9d7fcb7f0d867048f285441e574b
yanJuicy <hanjub3@gmail.com>
2022-11-18 10:59:50 +0900

    [programmers] 수박수박수박수박수박수? 10분

---
+++ java/CodingTest/src/programmers/수박수박수박수박수박수/Solution.java
@@ -0,0 +1,9 @@
+package programmers.수박수박수박수박수박수;
+
+public class Solution {
+
+    public String solution(int n) {
+        String answer = "수박".repeat(5000);
+        return answer.substring(0, n);
+    }
+}
