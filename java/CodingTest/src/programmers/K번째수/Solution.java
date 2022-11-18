27767823b7e31db16eca8194ce8427092ada9bdd
yanJuicy <hanjub3@gmail.com>
2022-11-19 02:09:37 +0900

    [programmers] K번째수 10분

--- java/CodingTest/src/programmers/K번째수/Solution.java
+++ java/CodingTest/src/programmers/K번째수/Solution.java
@@ -7,8 +7,8 @@ public class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
-        for (int i=0; i<commands.length; i++) {
-            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+        for (int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             Arrays.sort(arr);
             answer[i] = arr[commands[i][2] - 1];
         }
