aa093b63722beca19c456acc77525d596d3f55f7
yanJuicy <hanjub3@gmail.com>
2022-11-19 02:19:37 +0900

    [programmers] 나머지가 1이 되는 수 찾기 15분

---
+++ java/CodingTest/src/programmers/나머지가_1이_되는_수_찾기/Solution.java
@@ -0,0 +1,15 @@
+package programmers.나머지가_1이_되는_수_찾기;
+
+public class Solution {
+
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
