72bf3146f70f93f89165be989233e61700296b68
yanJuicy <hanjub3@gmail.com>
2022-11-18 13:16:25 +0900

    [programmers] 자릿수 더하기 1분

---
+++ java/CodingTest/src/programmers/자릿수_더하기/Solution.java
@@ -0,0 +1,15 @@
+package programmers.자릿수_더하기;
+
+public class Solution {
+
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+
+}
