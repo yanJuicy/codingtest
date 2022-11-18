732fdfa56fb57307883e3fa692c24ac432840d48
yanJuicy <hanjub3@gmail.com>
2022-11-18 19:21:32 +0900

    [programmers] 소수 만들기 30분

--- java/CodingTest/src/programmers/소수만들기/Solution.java
+++ java/CodingTest/src/programmers/소수만들기/Solution.java
@@ -1,51 +1,53 @@
 package programmers.소수만들기;
 
-public class Main {
+public class Solution {
 
     static int result = 0;
+
     public int solution(int[] nums) {
         int answer = 0;
-        boolean[] primes = getPrime();
+        boolean[] primes = primeEratostenes();
         boolean[] checked = new boolean[nums.length];
         combination(nums, checked, 0, nums.length, 3, primes);
         answer = result;
         return answer;
     }
 
-    private void combination(int[] nums, boolean[] checked, int index, int n, int r, boolean[] primes) {
+    private void combination(int[] nums, boolean[] checked, int index, int n, int r,
+            boolean[] primes) {
         if (r == 0) {
             int sum = 0;
-            for (int i=0; i<nums.length; i++) {
-                if (checked[i] == true) {
+            for (int i = 0; i < nums.length; i++) {
+                if (checked[i]) {
                     sum += nums[i];
                 }
             }
-            if (primes[sum] == false)
-               result++;
+
+            if (!primes[sum]) {
+                result++;
+            }
             return;
         }
 
-        for (int i=index; i<n; i++) {
+        for (int i = index; i < n; i++) {
             checked[i] = true;
             combination(nums, checked, i + 1, n, r - 1, primes);
             checked[i] = false;
         }
     }
 
-    private boolean[] getPrime() {
+    private boolean[] primeEratostenes() {
         boolean[] primes = new boolean[50001];
         primes[1] = true;
-        for (int i=2; i<=50000; i++) {
-            if (primes[i] == true)
+        for (int i = 2; i <= 50000; i++) {
+            if (primes[i] == true) {
                 continue;
+            }
+
             for (int j = i + i; j <= 50000; j += i) {
                 primes[j] = true;
             }
         }
         return primes;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Main().solution(new int[] {1, 2, 3, 4}));
     }
 }
