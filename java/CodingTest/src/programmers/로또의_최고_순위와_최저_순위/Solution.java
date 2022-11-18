0ba56d8060b42890a036f98d237b2083484071ff
yanJuicy <hanjub3@gmail.com>
2022-11-18 17:40:41 +0900

    [programmers] 로또의 최고 순위와 최저 순위 10분

--- java/CodingTest/src/programmers/로또의_최고_순위와_최저_순위/Solution.java
+++ java/CodingTest/src/programmers/로또의_최고_순위와_최저_순위/Solution.java
@@ -6,8 +6,8 @@ import java.util.Set;
 public class Solution {
 
     public int[] solution(int[] lottos, int[] win_nums) {
-        int high = 0;
-        int low = 0;
+        int highHit = 0;
+        int lowHit = 0;
         int unknowns = 0;
         int count = 0;
 
@@ -25,22 +25,22 @@ public class Solution {
             }
         }
 
-        high = count + unknowns > 6 ? 6 : count + unknowns;
-        low = count;
+        highHit = count + unknowns > 6 ? 6 : count + unknowns;
+        lowHit = count;
         int[] rank = {6, 5, 4, 3, 2};
 
-        int a = 6;
-        int b = 6;
+        int highRank = 6;
+        int lowRank = 6;
         for (int i = 0; i < rank.length; i++) {
-            if (high == rank[i]) {
-                a = i + 1;
+            if (highHit == rank[i]) {
+                highRank = i + 1;
             }
-            if (low == rank[i]) {
-                b = i + 1;
+            if (lowHit == rank[i]) {
+                lowRank = i + 1;
             }
         }
 
-        int[] answer = {a, b};
+        int[] answer = {highRank, lowRank};
         return answer;
     }
 }
