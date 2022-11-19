b51f37772ada55e07cf68316a054a3d42d2c5214
yanJuicy <hanjub3@gmail.com>
2022-11-19 18:49:39 +0900

    [programmers] [1차] 비밀지도 20분

---
+++ java/CodingTest/src/programmers/_1차_비밀지도/Solution.java
@@ -0,0 +1,21 @@
+package programmers._1차_비밀지도;
+
+public class Solution {
+
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder(Integer.toBinaryString(arr1[i] | arr2[i]));
+            while (sb.length() < n) {
+                sb.insert(0, '0');
+            }
+            String mapRow = sb.toString();
+            mapRow = mapRow.replaceAll("0", " ");
+            mapRow = mapRow.replaceAll("1", "#");
+            answer[i] = mapRow;
+        }
+
+        return answer;
+    }
+}
