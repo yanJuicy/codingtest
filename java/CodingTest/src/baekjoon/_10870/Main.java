2956c9a3391b3e3a1b99e412d99c73aaf57d57c9
Yanjoo <hanjub3@gmail.com>
2023-06-20 15:47:47 +0900

    [BOJ] 10870 피보나치 수 5 5분

---
+++ java/CodingTest/src/baekjoon/_10870/Main.java
@@ -0,0 +1,26 @@
+package baekjoon._10870;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		if (n == 0) {
+			System.out.println(0);
+			return;
+		}
+
+		int[] dp = new int[n + 1];
+		dp[0] = 0;
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		System.out.println(dp[n]);
+	}
+}
