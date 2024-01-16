11409bfc33b1113098257fc5ae7df8c9f010f537
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-01-16 14:10:58 +0900

    [level Lv.2] Title: 주식가격, Time: 30.94 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42584. 주식가격/주식가격.py
@@ -0,0 +1,18 @@
+def solution(prices):
+    length = len(prices)
+    
+    answer = [0] * length
+    
+    stack = []
+    for i in range(0, length):
+        while stack and prices[stack[-1]] > prices[i]:
+            top = stack.pop()
+            answer[top] = i - top
+        
+        stack.append(i)
+    
+    while stack:
+        top = stack.pop()
+        answer[top] = length - top - 1
+    
+    return answer
