b7a9e2990abe405df293b93053081afe7b34fb4b
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-01-25 14:43:34 +0900

    [level 2] Title: 올바른 괄호, Time: 7.81 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    stack = []
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        else:
+            if stack and stack[-1] == "(":
+                stack.pop()
+            else:
+                return False
+    
+    if stack:
+        return False
+
+    return True
