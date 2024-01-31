912df3a9cbb72c8e240ce304311eb91c67a9878b
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-01-31 15:30:39 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,11 @@
+def solution(cards1, cards2, goal):
+    for goal_word in goal:
+        if cards1 and goal_word == cards1[0]:
+            cards1.pop(0)
+        elif cards2 and goal_word == cards2[0]:
+            cards2.pop(0)
+        else:
+            return 'No'
+    
+    
+    return 'Yes'
