4ca939f23a10838f0f91fd2c8756d2336adb68ea
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-03-15 10:42:24 +0900

    [level 3] Title: 양과 늑대, Time: 0.42 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -0,0 +1,23 @@
+def solution(info, edges):
+    answer = 0
+    
+    tree = [[] for _ in range(len(info))]
+    for edge in edges:
+        parent, child = edge
+        tree[parent].append(child)
+    
+    q = [(0, 1, 0, set())]  # 현재 위치, 양의 수, 늑대의 수, 방문한 노드 집합
+    
+    while q:
+        current_node, sheep_count, wolf_count, visited = q.pop(0)
+        answer = max(answer, sheep_count)
+        visited.update(tree[current_node])
+        
+        for next_node in visited:
+            if info[next_node]:
+                if sheep_count > wolf_count + 1:
+                    q.append((next_node, sheep_count, wolf_count + 1, visited - {next_node}))
+            else:
+                q.append((next_node, sheep_count + 1, wolf_count, visited - {next_node}))
+            
+    return answer
