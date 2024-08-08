5374d8fbde7fda731d52d46fc4421b2af32c696f
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-08-09 08:56:05 +0900

    [Silver I] Title: 회의실 배정, Time: 520 ms, Memory: 43756 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int n;
+    private static int[][] a;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+    }
+
+    private static void solve() {
+        Arrays.sort(a, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1];
+            }
+        });
+
+        int end = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i][0] >= end) {
+                end = a[i][1];
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    private static void input() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bufferedReader.readLine());
+        a = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+            a[i][0] = Integer.parseInt(st.nextToken());
+            a[i][1] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+}
