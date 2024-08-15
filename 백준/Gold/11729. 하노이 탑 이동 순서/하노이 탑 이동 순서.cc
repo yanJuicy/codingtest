ae69d2c6663b2336ac0877b84f602e1d3702f03f
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-08-15 11:09:42 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 140 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void func(int a, int b, int n)
+{
+	if (n==0) return;
+	func(a, 6-a-b, n-1);
+	cout << a << ' ' << b << '\n';
+	func(6-a-b, b, n-1);
+}
+
+int main()
+{
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	cout << (1 << n) - 1 << '\n';
+	func(1, 3, n);
+
+
+	return 0;
+}
