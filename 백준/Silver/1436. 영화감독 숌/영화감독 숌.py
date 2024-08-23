85ed785dca3e64ad18c199921a05b169062e222d
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-08-23 09:55:12 +0900

    [Silver V] Title: 영화감독 숌, Time: 600 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,15 @@
+current = 0
+answer = 1
+
+n = int(input())
+
+while True:
+    if "666" in str(answer):
+        current += 1
+
+    if current == n:
+        break
+
+    answer += 1
+
+print(answer)
