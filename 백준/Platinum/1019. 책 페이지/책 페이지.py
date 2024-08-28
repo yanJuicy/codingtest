ed37e6ce01d455463c23d10442abaa5ade659cff
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-08-28 09:46:42 +0900

    [Platinum V] Title: 책 페이지, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Platinum/1019. 책 페이지/책 페이지.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+answer = [0] * 10
+rest_add_nums = 0
+rest_add_num_zero = 1
+digit = 1
+
+while n > 0:
+    cur = n % 10
+    n //= 10
+
+    answer[0] -= digit
+    for i in range(cur):
+        answer[i] += (n + 1) * digit
+    answer[cur] += n * digit + rest_add_nums + rest_add_num_zero
+    for i in range(cur + 1, 9 + 1):
+        answer[i] += n * digit
+
+    rest_add_nums += cur * digit
+    digit *= 10
+
+
+print(*answer)
