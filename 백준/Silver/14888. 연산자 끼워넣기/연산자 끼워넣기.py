b75e1c5838c39898eecc6287846b0d5167673f7d
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-09-14 11:30:36 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,42 @@
+def backtracking(index, sum):
+    global minAns
+    global maxAns
+
+    if index == n - 1:
+        if minAns > sum:
+            minAns = sum
+        if maxAns < sum:
+            maxAns = sum
+        return
+
+    for i in range(4):
+        temp = sum
+        if operator[i] == 0:
+            continue
+        if i == 0:
+            sum += numArr[index + 1]
+        elif i == 1:
+            sum -= numArr[index + 1]
+        elif i == 2:
+            sum *= numArr[index + 1]
+        else:
+            if sum < 0: 
+                sum = abs(sum) // numArr[index + 1] * - 1
+            else:
+                sum //= numArr[index + 1]
+        
+        operator[i] -= 1
+        backtracking(index + 1, sum)
+        operator[i] += 1
+        sum = temp
+
+
+n = int(input())
+numArr = list(map(int, input().split()))
+operator = list(map(int, input().split()))
+minAns = float('Inf')
+maxAns = float('-Inf')
+
+backtracking(0, numArr[0])
+print(maxAns)
+print(minAns)
