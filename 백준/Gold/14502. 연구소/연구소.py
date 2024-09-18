1778f09a319b4b5ab4e40cc29400a3ea3b968fa6
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-09-18 12:16:24 +0900

    [Gold IV] Title: 연구소, Time: 5132 ms, Memory: 151232 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,50 @@
+n, m = map(int, input().split())
+graph = []
+temp = [[0] * m for _ in range(n)]
+
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+dx = [0, 0, 1, -1]
+dy = [-1, 1, 0, 0]
+
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = 2
+                virus(nx, ny)
+
+def get_score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
+
+result = 0
+def dfs(count):
+    global result
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = graph[i][j]
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i, j)
+        result = max(result, get_score())
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                dfs(count + 1)
+                graph[i][j] = 0
+          
+dfs(0)
+print(result)
