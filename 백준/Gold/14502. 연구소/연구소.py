720904d01d0e651916750bf5707653ecef136ca6
LeeHanju <43159295+yanJuicy@users.noreply.github.com>
2024-09-18 13:42:52 +0900

    [Gold IV] Title: 연구소, Time: 1540 ms, Memory: 117296 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,50 +1,63 @@
-n, m = map(int, input().split())
-graph = []
-temp = [[0] * m for _ in range(n)]
+from collections import deque
 
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
 
-dx = [0, 0, 1, -1]
-dy = [-1, 1, 0, 0]
+def bfs():
+    global answer
+    bfs_visited = [[0 for _ in range(m)] for _ in range(n)]
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+    q = deque()
 
-def virus(x, y):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx >= 0 and nx < n and ny >= 0 and ny < m:
-            if temp[nx][ny] == 0:
-                temp[nx][ny] = 2
-                virus(nx, ny)
-
-def get_score():
-    score = 0
     for i in range(n):
         for j in range(m):
-            if temp[i][j] == 0:
-                score += 1
-    return score
+            bfs_visited[i][j] = lab[i][j]
+            if lab[i][j] == 2:
+                q.append([i, j])
+                bfs_visited[i][j] = 1
 
-result = 0
-def dfs(count):
-    global result
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = graph[i][j]
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 2:
-                    virus(i, j)
-        result = max(result, get_score())
+    while q:
+        r, c = q.popleft()
+
+        for d in range(4):
+            next_r = r + dy[d]
+            next_c = c + dx[d]
+            if (
+                0 <= next_r < n
+                and 0 <= next_c < m
+                and not lab[next_r][next_c]
+                and not bfs_visited[next_r][next_c]
+            ):
+                q.append([next_r, next_c])
+                bfs_visited[next_r][next_c] = 1
+
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if not lab[i][j] and not bfs_visited[i][j]:
+                cnt += 1
+
+    answer = max(answer, cnt)
+
+
+def backtracking(wall_count):
+    if wall_count == 3:
+        bfs()
         return
 
     for i in range(n):
         for j in range(m):
-            if graph[i][j] == 0:
-                graph[i][j] = 1
-                dfs(count + 1)
-                graph[i][j] = 0
-          
-dfs(0)
-print(result)
+            if not lab[i][j] and not backtracking_visited[i][j]:
+                backtracking_visited[i][j] = 1
+                lab[i][j] = 1
+                backtracking(wall_count + 1)
+                backtracking_visited[i][j] = 0
+                lab[i][j] = 0
+
+
+answer = 0
+n, m = map(int, input().split())
+lab = [list(map(int, input().split())) for _ in range(n)]
+backtracking_visited = [[0 for _ in range(m)] for _ in range(n)]
+
+backtracking(0)
+print(answer)
